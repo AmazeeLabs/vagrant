e3e654db9afca8ad84ac670027c6f66be7432765
Michael Schmid <michael@amazeelabs.com>
2015-03-02 20:26:46 +0100

    updated to different (and faster) xhprof version

--- vagrant/xhprof/recipes/default.rb
+++ vagrant/xhprof/recipes/default.rb
@@ -12,14 +12,14 @@ directory "/opt/xhprof" do
 end
 
 git "/opt/xhprof" do
-    repository "git@github.com:gajus/xhprof.io.git"
+    repository "git@github.com:AmazeeLabs/xhprof.git"
     revision "master"
     user "vagrant"
     action :sync
 end
 
-template "/opt/xhprof/xhprof/includes/config.inc.php" do
-    source "config.inc.php.erb"
+template "/opt/xhprof/xhprof_lib/config.php" do
+    source "config.php.erb"
     mode 0644
 end
 
@@ -27,7 +27,30 @@ bash "Creating xhprof database" do
     user "vagrant"
     code <<-EOH
         mysql -u root -e 'create database xhprof';
-        mysql -u root 'xhprof' < /opt/xhprof/setup/database.sql
+        mysql -u root 'xhprof' -e ' CREATE TABLE `details` (
+                                    `id` char(17) NOT NULL,
+                                    `url` varchar(255) default NULL,
+                                    `c_url` varchar(255) default NULL,
+                                    `timestamp` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+                                    `server name` varchar(64) default NULL,
+                                    `perfdata` MEDIUMBLOB,
+                                    `type` tinyint(4) default NULL,
+                                    `cookie` BLOB,
+                                    `post` BLOB,
+                                    `get` BLOB,
+                                    `pmu` int(11) unsigned default NULL,
+                                    `wt` int(11) unsigned default NULL,
+                                    `cpu` int(11) unsigned default NULL,
+                                    `server_id` char(3) NOT NULL default "t11",
+                                    `aggregateCalls_include` varchar(255) DEFAULT NULL,
+                                    PRIMARY KEY  (`id`),
+                                    KEY `url` (`url`),
+                                    KEY `c_url` (`c_url`),
+                                    KEY `cpu` (`cpu`),
+                                    KEY `wt` (`wt`),
+                                    KEY `pmu` (`pmu`),
+                                    KEY `timestamp` (`timestamp`)
+                                    ) ENGINE=MyISAM DEFAULT CHARSET=utf8;';
     EOH
 end
 
